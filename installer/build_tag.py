c137028a98cd762a4e93950fbde085969500999e
Marcin Zdun <mzdun@midnightbits.com>
2015-04-10 10:18:54 +0200

    Build tagger

---
+++ installer/build_tag.py
@@ -0,0 +1,12 @@
+#!/usr/python
+
+import os
+from subprocess import call, check_output
+ver = check_output([ "python", "version.py", "../apps/Tasks/src/version.h",
+	"PROGRAM_VERSION_MAJOR,PROGRAM_VERSION_MINOR,PROGRAM_VERSION_PATCH,PROGRAM_VERSION_BUILD",
+	"PROGRAM_VERSION_BUILD"])
+VERSION = ver.strip()
+
+call(["git","add","../apps/Tasks/src/version.h"])
+call(["git","commit","-m","Build for version %s" % VERSION])
+call(["git","tag","tag-build-v%s" % VERSION])
