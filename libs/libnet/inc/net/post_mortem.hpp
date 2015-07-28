06d4eca265470855562e21046e1a54b8fdc58897
Marcin Zdun <mzdun@midnightbits.com>
2015-07-28 21:52:03 +0200

    [JIRDESK-85] Writing MINIDUMPs in temp dir

--- libs/libnet/inc/net/post_mortem.hpp
+++ libs/libnet/inc/net/post_mortem.hpp
@@ -26,6 +26,7 @@
 #define __POST_MORTEM_HPP__
 
 #include <thread>
+#include <net/filesystem.hpp>
 #ifdef WIN32
 #	include <windows.h>
 #   pragma warning(push)
@@ -41,7 +42,31 @@ namespace pm
 #ifdef WIN32
 
     class Win32PostMortemSupport {
-    public:
+        static fs::path tempDir() {
+            auto size = GetTempPath(0, nullptr);
+            if (!size)
+                return fs::current_path();
+
+            ++size;
+            std::unique_ptr<WCHAR[]> path{ new WCHAR[size] };
+            if (!GetTempPath(size, path.get()))
+                return fs::current_path();
+
+            return path.get();
+        }
+
+        static std::wstring buildPath() {
+            auto temp = tempDir();
+            temp /= "MiniDump.dmp";
+            temp.make_preferred();
+            return temp.wstring();
+        }
+
+        static LPCWSTR dumpPath() {
+            static std::wstring path = buildPath();
+            return path.c_str();
+        }
+
         static void Write(_EXCEPTION_POINTERS* ep) {
             auto mdt = static_cast<MINIDUMP_TYPE>(
                 MiniDumpWithDataSegs |
@@ -53,7 +78,7 @@ namespace pm
                 MiniDumpWithUnloadedModules
                 );
 
-            auto file = CreateFileW(L"MiniDump.dmp", GENERIC_READ | GENERIC_WRITE,
+            auto file = CreateFileW(dumpPath(), GENERIC_READ | GENERIC_WRITE,
                 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
 
             if (file && file != INVALID_HANDLE_VALUE) {
@@ -63,17 +88,24 @@ namespace pm
             }
             TerminateProcess(GetCurrentProcess(), 1);
         }
+
+    public:
+        template <class Fn, class... Args>
+        static void Run(Fn&& fn, Args&&... args) {
+            (void)dumpPath(); // build file path now for any OOM later...
+            _EXCEPTION_POINTERS* exception = nullptr;
+            __try {
+                fn(std::forward<Args>(args)...);
+            }
+            __except (exception = GetExceptionInformation(), EXCEPTION_EXECUTE_HANDLER) {
+                Write(exception);
+            }
+        }
     };
 
     template <class Fn, class... Args>
     inline void PostMortemSupport(Fn&& fn, Args&&... args) {
-        _EXCEPTION_POINTERS* exception = nullptr;
-        __try {
-            fn(std::forward<Args>(args)...);
-        }
-        __except (exception = GetExceptionInformation(), EXCEPTION_EXECUTE_HANDLER) {
-            Win32PostMortemSupport::Write(exception);
-        }
+        Win32PostMortemSupport::Run(std::forward<Fn>(fn), std::forward<Args>(args)...);
     }
 
 #else // !WIN32
